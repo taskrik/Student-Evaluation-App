54248c89fa4b49436b5676f4daed252ffeb0b081
tasos <tasos.krikonis@gmail.com>
2018-09-14 12:52:56 +0200

    fixed action bugs

--- client/src/middleware.js
+++ client/src/middleware.js
@@ -1,9 +1,9 @@
-import {LOGIN_SUCCESS, USER_LOGOUT} from './actions/login'
+import {USER_LOGIN_SUCCESS, USER_LOGOUT} from './actions/login'
 import {localStorageJwtKey} from './constants'
 
 export const storeJwt = store => next => action => {
   try {
-    if (action.type === LOGIN_SUCCESS) {
+    if (action.type === USER_LOGIN_SUCCESS) {
       localStorage.setItem(localStorageJwtKey, action.payload.jwt)
     }
     if (action.type === USER_LOGOUT) {
